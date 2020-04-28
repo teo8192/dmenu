66e7284adaa2761033bd8fab48d127a5395b9d10
Teo <teodor@dahlknutsen.no>
2020-04-28 19:01:43 +0200

    Instant match

--- dmenu.c
+++ dmenu.c
@@ -296,6 +296,13 @@ fuzzymatch(void)
 		free(fuzzymatches);
 	}
 	curr = sel = matches;
+
+	if (instant && matches && matches==matchend) {
+		puts(matches->text);
+		cleanup();
+		exit(0);
+	}
+
 	calcoffsets();
 }
 
@@ -354,6 +361,13 @@ match(void)
 		matchend = substrend;
 	}
 	curr = sel = matches;
+
+	if (instant && matches && matches==matchend && !lsubstr) {
+		puts(matches->text);
+		cleanup();
+		exit(0);
+	}
+
 	calcoffsets();
 }
 
@@ -897,7 +911,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiPv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bfinPv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
 }
@@ -924,6 +938,8 @@ main(int argc, char *argv[])
 			fstrstr = cistrstr;
 		} else if (!strcmp(argv[i], "-P"))   /* is the input a password */
 		        passwd = 1;
+		else if  (!strcmp(argv[i], "-n")) // instant select only match
+			instant = 1;
 		else if (i + 1 == argc)
 			usage();
 		/* these options take one argument */
